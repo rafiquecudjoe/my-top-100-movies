0421f23a9b2b68367680535360058965e940f2d9
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2022-11-19 13:41:41 +0000

    minor update

--- src/common/entities/nellys-coin-user.entity.ts
+++
@@ -1,9 +0,0 @@
-export class NellysCoinUser {
-  username: string;
-  first_name: string;
-  last_name: string;
-  email_address: string;
-  preferred_language: string;
-  account_status: string;
-  customer_type: string;
-}
